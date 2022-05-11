60ae386a90664c71463b2d704ea15d9c37600ca0
mipmip <mipmip@users.noreply.github.com>
2022-05-11 11:46:21 +0000

    deploy: 3e7401764f284433b539501b62f9886e6109b961

---
+++ _next/static/MAG3uOwnv_gchKitBr1Jh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/140-83c0dc182ccf473b.js","static/chunks/pages/index-c542d5adae75e4ae.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
