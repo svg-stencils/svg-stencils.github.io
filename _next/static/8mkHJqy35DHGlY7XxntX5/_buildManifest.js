becd130f5d3396725b13f2493aad90ada8a39a00
mipmip <mipmip@users.noreply.github.com>
2022-05-18 22:21:38 +0000

    deploy: e7c210f38c8281d3adac6cffeb9e63366546d0e9

---
+++ _next/static/8mkHJqy35DHGlY7XxntX5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/424-25955f43e64a2aff.js","static/chunks/pages/index-89d853cbcdba7633.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
