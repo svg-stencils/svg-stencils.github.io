753b1d654cf72cb0fb5ab7db94256ddaf238c11b
mipmip <mipmip@users.noreply.github.com>
2022-05-25 10:37:00 +0000

    deploy: 7a2c3a196ba2b1817952e6083524eae2109e7251

--- _next/static/XxPOo1ek8in6-GwS9tYGC/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/74-20a5b9c25778f156.js","static/chunks/pages/index-3caf4e00de55cf69.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/vztzmqvrPJJHEOCjZgJF7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/74-20a5b9c25778f156.js","static/chunks/pages/index-5455471d57182270.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
